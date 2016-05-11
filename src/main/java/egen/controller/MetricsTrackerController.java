e2ee14655c7db21e26646fa22088f728b5d84998
abrolbhavna <abrolbhavna@users.noreply.github.com>
2016-05-10 21:09:52 -0500

    Update MetricsTrackerController.java

--- src/main/java/egen/controller/MetricsTrackerController.java
+++ src/main/java/egen/controller/MetricsTrackerController.java
@@ -2,7 +2,7 @@ package egen.controller;
 
 import java.text.ParseException;
 import java.util.List;
-
+import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,29 +23,32 @@ public class MetricsTrackerController  {
 	private MetricsTrackerService metricsTrackerService;
 
 	/*
-	 * this is the API that will consume data from the sensor emulator and save in metrics database
+	 * this is the API that will consume data from the sensor emulator and calls services which saves the data
+	 * in mongoDB metrics collection
 	 */
 	
 	@RequestMapping(value = "/metrics", method = RequestMethod.POST, params = "application/json")
-	public Metrics create(@RequestParam(value="timeStamp") String timeStamp, @RequestParam (value="value") String value){
+	public Response  create(@RequestParam(value="timeStamp") String timeStamp, @RequestParam (value="value") String value){
 		Metrics result = metricsTrackerService.create(timeStamp, value);		
-		return result;
+		return Response.status(200).entity(result).build();
 	}
 
 	/*
 	 * reads all metrics that are stored in the database
 	 */
 	@RequestMapping(value = "/read", method = RequestMethod.GET)
-	public Iterable<Metrics> read() {
-		return metricsTrackerService.read();
+	public Response  read() {
+		Iterable<Metrics> list = metricsTrackerService.read();
+		return Response.status(200).entity(list).build();
 	}
 
 	/*
 	 * reads all alerts that are created between the given two timestamps
 	 */
 	@RequestMapping(value = "/readByTimeRange", method = RequestMethod.GET)
-	public List<Metrics> readByTimeRange(@PathVariable String startTime, @PathVariable String endTime) throws ParseException {
-		return metricsTrackerService.readByTimeRange(startTime, endTime);
+	public Response readByTimeRange(@PathVariable String startTime, @PathVariable String endTime) throws ParseException {
+		List<Metrics> listByTimeRange = metricsTrackerService.readByTimeRange(startTime, endTime);
+		return Response.status(200).entity(listByTimeRange).build();
 	}
 
 
