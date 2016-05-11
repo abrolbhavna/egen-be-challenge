c9e63be47963cdc0c09695d4a35571a8731524f9
abrolbhavna <abrolbhavna@users.noreply.github.com>
2016-05-10 21:12:47 -0500

    Update AlertsController.java

--- src/main/java/egen/controller/AlertsController.java
+++ src/main/java/egen/controller/AlertsController.java
@@ -1,7 +1,8 @@
 package egen.controller;
 
 import java.text.ParseException;
-import java.util.List;
+
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,16 +26,18 @@ public class AlertsController  {
 	 * reads all alerts that are stored in the database
 	 */
 	@RequestMapping(value = "/read", method = RequestMethod.GET)
-	public Iterable<Alerts> read() {
-		return alertTrackerService.read();
+	public Response read() {
+		Iterable<Alerts> list = alertTrackerService.read();
+		return Response.status(200).entity(list).build();
 	}
 
 	/*
 	 * reads all alerts that are created between the given two timestamps
 	 */
 	@RequestMapping(value = "/readByTimeRange", method = RequestMethod.GET)
-	public List<Alerts> readByTimeRange(@PathVariable String startTime, @PathVariable String endTime) throws ParseException {
-		return alertTrackerService.readByTimeRange(startTime, endTime);
+	public Response readByTimeRange(@PathVariable String startTime, @PathVariable String endTime) throws ParseException {
+		Iterable<Alerts> listByTimeRange = alertTrackerService.readByTimeRange(startTime, endTime);
+		return Response.status(200).entity(listByTimeRange).build();
 	}
 
 
